b4657c36b643f9e314e8a54597b361139114fff3
angular-cli <angular-cli@angular.io>
2016-06-05 17:42:50 +0700

    chore: initial commit from angular-cli

---
+++ src/app/material.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'material-app',
+  templateUrl: 'material.component.html',
+  styleUrls: ['material.component.css']
+})
+export class MaterialAppComponent {
+  title = 'material works!';
+}
