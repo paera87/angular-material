fcc8e48027fb70e6fe0e66b9297b54afb45770d3
Par <paer1301@student.miun.se>
2016-06-05 18:08:20 +0700

    added button

--- src/app/material.component.ts
+++ src/app/material.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 @Component({
   moduleId: module.id,
   selector: 'material-app',
   templateUrl: 'material.component.html',
-  styleUrls: ['material.component.css']
+  styleUrls: ['material.component.css'],
+  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES]
 })
 export class MaterialAppComponent {
   title = 'material works!';
